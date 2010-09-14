32858d829184a521516ba7a2452283d5d04ff8ca
smimram@aec24677-d710-0410-a355-ac75e2bdf181 <smimram@aec24677-d710-0410-a355-ac75e2bdf181>
2010-09-14 08:19:04 +0000

    Example to display an image.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%407632

---
+++ examples/display_image/display.ml
@@ -0,0 +1,19 @@
+module Img = Image.RGBA8
+
+let read_PPM ?alpha fname =
+  let ic = open_in_bin fname in
+  let len = in_channel_length ic in
+  let data = String.create len in
+  really_input ic data 0 len;
+  close_in ic;
+  Img.of_PPM ?alpha data
+
+let () =
+  let fname = Sys.argv.(1) in
+  let img = read_PPM fname in
+  (* let img = Img.Scale.create ~kind:Img.Scale.Bilinear img 500 500 in *)
+  let w,h = Img.dimensions img in
+  Graphics.open_graph "";
+  Graphics.resize_window w h;
+  Graphics.draw_image (Graphics.make_image (Img.to_int_image img)) 0 0;
+  ignore (Graphics.wait_next_event [Graphics.Key_pressed])
