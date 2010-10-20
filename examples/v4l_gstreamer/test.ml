173db0cc1d7598d282eae756b5c666728a7265b1
smimram@aec24677-d710-0410-a355-ac75e2bdf181 <smimram@aec24677-d710-0410-a355-ac75e2bdf181>
2010-10-20 19:25:11 +0000

    Playing my webcam with gstreamer.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%407863

--- examples/v4l_gstreamer/test.ml
+++ examples/v4l_gstreamer/test.ml
@@ -2,13 +2,18 @@ open Gstreamer
 
 let width = 320
 let height = 240
+let fps = 10
 
 let src = "videotestsrc"
+let src = "v4l2src device=/dev/video0"
+
+let pipeline = Printf.sprintf "%s ! ffmpegcolorspace ! videoscale ! appsink drop=true name=sink caps=\"video/x-raw-rgb,width=%d,height=%d,pixel-aspect-ratio=1/1,bpp=(int)24,depth=(int)24,endianness=(int)4321,red_mask=(int)0xff0000,green_mask=(int)0x00ff00,blue_mask=(int)0x0000ff,framerate=(fraction)10/1\"" src width height
 
 let () =
   Gstreamer.init ();
   Printf.printf "%s\n%!" (version_string ());
-  let bin =  Pipeline.parse_launch (src ^ " ! ffmpegcolorspace ! videoscale ! appsink name=sink caps=\"video/x-raw-rgb,width=320,height=240,pixel-aspect-ratio=1/1,bpp=(int)24,depth=(int)24,endianness=(int)4321,red_mask=(int)0xff0000, green_mask=(int)0x00ff00, blue_mask=(int)0x0000ff\"") in
+  Printf.printf "%s\n%!" pipeline;
+  let bin =  Pipeline.parse_launch pipeline in
 
   let sink = Bin.get_by_name (Bin.of_element bin) "sink" in
 
@@ -19,7 +24,6 @@ let () =
   while true do
     let b = App_sink.pull_buffer (App_sink.of_element sink) in
     let blen = Bigarray.Array1.dim b in
-    Printf.printf "%d\n%!" (blen / (width * height));
     let img = Image.Generic.make_rgb Image.Generic.Pixel.RGB24 width height b in
     let out = Image.RGBA8.create width height in
     Image.Generic.convert ~copy:true ~proportional:true img (Image.Generic.of_RGBA8 out);
