91c9ea5141a2722299bc5e8f9d6d8a997a7f70c0
smimram@aec24677-d710-0410-a355-ac75e2bdf181 <smimram@aec24677-d710-0410-a355-ac75e2bdf181>
2010-10-20 19:00:16 +0000

    Correctly decoding videotestsrc.
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%407862

--- examples/v4l_gstreamer/test.ml
+++ examples/v4l_gstreamer/test.ml
@@ -3,10 +3,13 @@ open Gstreamer
 let width = 320
 let height = 240
 
+let src = "videotestsrc"
+
 let () =
   Gstreamer.init ();
   Printf.printf "%s\n%!" (version_string ());
-  let bin = Pipeline.parse_launch "videotestsrc ! videoscale ! appsink max_buffers=2 drop=true caps=video/x-raw-rgb,width=320,height=240,framerate='(fraction)'10/1,bpp=32,depth=24 name=sink" in
+  let bin =  Pipeline.parse_launch (src ^ " ! ffmpegcolorspace ! videoscale ! appsink name=sink caps=\"video/x-raw-rgb,width=320,height=240,pixel-aspect-ratio=1/1,bpp=(int)24,depth=(int)24,endianness=(int)4321,red_mask=(int)0xff0000, green_mask=(int)0x00ff00, blue_mask=(int)0x0000ff\"") in
+
   let sink = Bin.get_by_name (Bin.of_element bin) "sink" in
 
   let sdl = new MMSDL.writer_to_screen width height in
@@ -17,7 +20,7 @@ let () =
     let b = App_sink.pull_buffer (App_sink.of_element sink) in
     let blen = Bigarray.Array1.dim b in
     Printf.printf "%d\n%!" (blen / (width * height));
-    let img = Image.Generic.make_rgb Image.Generic.Pixel.RGB32 width height b in
+    let img = Image.Generic.make_rgb Image.Generic.Pixel.RGB24 width height b in
     let out = Image.RGBA8.create width height in
     Image.Generic.convert ~copy:true ~proportional:true img (Image.Generic.of_RGBA8 out);
     vid.(0) <- out;
