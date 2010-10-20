f2a8d923f5df2387ace7cf1ef3dcddd690685a48
smimram@aec24677-d710-0410-a355-ac75e2bdf181 <smimram@aec24677-d710-0410-a355-ac75e2bdf181>
2010-10-20 18:10:34 +0000

    I can see a reddish video test...
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%407861

--- examples/v4l_gstreamer/test.ml
+++ examples/v4l_gstreamer/test.ml
@@ -6,7 +6,7 @@ let height = 240
 let () =
   Gstreamer.init ();
   Printf.printf "%s\n%!" (version_string ());
-  let bin = Pipeline.parse_launch "videotestsrc ! appsink max_buffers=2 drop=true caps=video/x-raw-rgb,width=320,height=240,framerate='(fraction)'10/1,bpp=24,depth=24 name=sink" in
+  let bin = Pipeline.parse_launch "videotestsrc ! videoscale ! appsink max_buffers=2 drop=true caps=video/x-raw-rgb,width=320,height=240,framerate='(fraction)'10/1,bpp=32,depth=24 name=sink" in
   let sink = Bin.get_by_name (Bin.of_element bin) "sink" in
 
   let sdl = new MMSDL.writer_to_screen width height in
@@ -17,7 +17,7 @@ let () =
     let b = App_sink.pull_buffer (App_sink.of_element sink) in
     let blen = Bigarray.Array1.dim b in
     Printf.printf "%d\n%!" (blen / (width * height));
-    let img = Image.Generic.make_rgb Image.Generic.Pixel.RGB24 width height b in
+    let img = Image.Generic.make_rgb Image.Generic.Pixel.RGB32 width height b in
     let out = Image.RGBA8.create width height in
     Image.Generic.convert ~copy:true ~proportional:true img (Image.Generic.of_RGBA8 out);
     vid.(0) <- out;
