aa9b461c4088925ee3cffb80b8e0a5623f4fbfc8
Samuel Mimram <smimram@gmail.com>
2012-05-01 18:04:26 +0200

    Video player example.
    --HG--
    rename : ocaml-mm/examples/v4l_gstreamer/Makefile => ocaml-mm/examples/video_player/Makefile
    rename : ocaml-mm/examples/v4l_gstreamer/OCamlMakefile => ocaml-mm/examples/video_player/OCamlMakefile
    rename : ocaml-mm/examples/v4l_gstreamer/test.ml => ocaml-mm/examples/video_player/test.ml
    extra : rebase_source : 6f9ec46e6321653430239f7326208d3827c93458

---
+++ examples/video_player/test.ml
@@ -0,0 +1,32 @@
+open Gstreamer
+
+let width = 320
+let height = 240
+let fps = 24
+
+let src = "filesrc location=../test.wmv"
+
+let pipeline = Printf.sprintf "%s ! decodebin ! ffmpegcolorspace ! videoscale ! videorate ! appsink max-buffers=2 drop=true name=sink caps=\"video/x-raw-rgb,width=%d,height=%d,pixel-aspect-ratio=1/1,bpp=(int)24,depth=(int)24,endianness=(int)4321,red_mask=(int)0xff0000,green_mask=(int)0x00ff00,blue_mask=(int)0x0000ff,framerate=(fraction)%d/1\"" src width height fps
+
+let () =
+  Gstreamer.init ();
+  Printf.printf "%s\n%!" (version_string ());
+  Printf.printf "%s\n%!" pipeline;
+  let bin =  Pipeline.parse_launch pipeline in
+
+  let sink = Bin.get_by_name (Bin.of_element bin) "sink" in
+
+  let sdl = new MMSDL.writer_to_screen width height in
+  let vid = Video.create 1 in
+
+  ignore (Element.set_state bin State_playing);
+  while true do
+    let b = App_sink.pull_buffer (App_sink.of_element sink) in
+    let blen = Bigarray.Array1.dim b in
+    let img = Image.Generic.make_rgb Image.Generic.Pixel.RGB24 width height b in
+    let out = Image.RGBA32.create width height in
+    Image.Generic.convert ~copy:true ~proportional:true img (Image.Generic.of_RGBA32 out);
+    vid.(0) <- out;
+    sdl#write vid 0 1
+  done;
+  ignore (Element.set_state bin State_null)
