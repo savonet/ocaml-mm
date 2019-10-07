de474eb2b9f29aa20ae4749ab8edaae2ed2f08ea
Samuel Mimram <smimram@gmail.com>
2019-10-07 09:49:12 +0200

    Example compiling again.

--- examples/dictee/dictee.ml
+++ examples/dictee/dictee.ml
@@ -28,7 +28,7 @@ let () =
   let mid = new MIDI.IO.Writer.to_file f#sample_rate "out.mid" in
   let fft_n = 11 in
   let fft = FFT.init fft_n in
-  let blen = FFT.duration fft in
+  let blen = FFT.length fft in
   let buf = Audio.create f#channels blen in
   let agc = Audio.Effect.auto_gain_control f#channels f#sample_rate ~kup:0.9 ~kdown:0.7 ~rms_target:2. () in
   let adsr = Audio.Mono.Effect.ADSR.make f#sample_rate (0.02,0.01,0.9,0.05) in
@@ -37,10 +37,10 @@ let () =
   let prevnotes = ref [] in
   synth#set_volume 0.1;
   while !loop do
-    let r = f#read buf 0 blen in
-    agc#process buf 0 blen;
+    let r = f#read buf in
+    agc#process buf;
     loop := r <> 0;
-    let notes = FFT.notes f#sample_rate fft ~note_min:(Audio.Note.create 0 4) ~volume_min:0.01 ~filter_harmonics:false (Audio.to_mono buf) 0 blen in
+    let notes = FFT.notes f#sample_rate fft ~note_min:(Audio.Note.create 0 4) ~volume_min:0.01 ~filter_harmonics:false (Audio.to_mono buf) in
     let notes = List.sort (fun (_,v1) (_,v2) -> if v1 < v2 then 1 else -1) notes in
     let notes = list_head_n polyphony notes in
     (* Printf.printf "Notes: %d\n%!" (List.length notes); *)
@@ -48,12 +48,12 @@ let () =
     List.iter (fun (n,v) -> synth#note_off n 1.; mid#note_off mchan n (10. *. v)) (list_diff ncmp !prevnotes notes);
     List.iter (fun (n,v) -> synth#note_on n 1.; mid#note_on mchan n (10. *. v)) (list_diff ncmp notes !prevnotes);
     prevnotes := notes;
-    synth#fill_add buf 0 blen;
+    synth#fill_add buf;
     mid#advance blen;
-    Audio.amplify 2. buf 0 blen;
-    wav#write buf 0 blen;
+    Audio.amplify 2. buf;
+    wav#write buf;
     if oss_out then
-      oss#write buf 0 blen
+      oss#write buf
   done;
   wav#close;
   mid#close;
