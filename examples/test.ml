6e0341c8d3f12ef6f793504bc44be804cba01260
Samuel Mimram <smimram@gmail.com>
2022-01-25 08:46:09 +0100

    More tests.

--- examples/test.ml
+++ examples/test.ml
@@ -1,19 +1,27 @@
 open Mm_audio
 open Mm_image
 
-let time name f =
+let skip_long = false
+
+let test name f =
   Printf.printf "- %s... %!" name;
-  let t0 = Sys.time () in
   f ();
-  let t1 = Sys.time () in
-  Printf.printf "%.02f s\n%!" (t1 -. t0)
+  Printf.printf "ok\n%!"
+
+let time ?(skip=false) name f =
+  Printf.printf "- %s... %!" name;
+  if skip then Printf.printf "skipped\n%!" else
+    let t0 = Sys.time () in
+    f ();
+    let t1 = Sys.time () in
+    Printf.printf "%.02f s\n%!" (t1 -. t0)
 
 let () =
   Printf.printf "\n# Testing MM library\n\n%!"
 
 let () =
   Printf.printf "## Testing audio\n\n%!";
-  time "adding many buffers" (fun () ->
+  time ~skip:skip_long "adding many buffers" (fun () ->
       let a = Audio.create 2 44100 in
       for _ = 1 to 10000 do
         let b = Audio.create 2 44100 in
@@ -24,11 +32,22 @@ let () =
 
 let () =
   Printf.printf "## Testing video\n\n%!";
-  let a = Image.YUV420.create 640 480 in
-  Image.YUV420.blank a;
-  let b = Image.YUV420.create 64 64 in
-  Printf.printf "- adding images\n%!";
-  Image.YUV420.add b a;
-  Image.YUV420.add b ~x:10 ~y:10 a;
-  Image.YUV420.add b ~x:1000 ~y:1000 a;
+  test "fill buffer" (fun () ->
+      let a = Image.YUV420.create 10 10 in
+      Image.YUV420.fill a (0,0,0)
+    );
+  test "adding images" (fun () ->
+      let a = Image.YUV420.create 640 480 in
+      Image.YUV420.blank a;
+      Image.YUV420.fill a (10, 10, 10);
+      let b = Image.YUV420.create 64 64 in
+      Image.YUV420.fill b (10, 10, 10);
+      Image.YUV420.add b a;
+      Image.YUV420.add b ~x:10 ~y:10 a;
+      Image.YUV420.add b ~x:(-10) ~y:(-10) a;
+      Image.YUV420.add b ~x:1000 ~y:1000 a;
+    );
   Printf.printf "\n"
+
+let () =
+  Gc.full_major ()
