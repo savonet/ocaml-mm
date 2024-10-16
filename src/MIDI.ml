3292581d5c9a48594fb3815c770632dc96776a11
Samuel Mimram <smimram@gmail.com>
2024-10-16 16:41:35 +0200

    Cosmetic.

--- src/MIDI.ml
+++ src/MIDI.ml
@@ -38,12 +38,9 @@ type division = Ticks_per_quarter of int | SMPTE of int * int
 
 type event =
   | Note_off of Audio.Note.t * float
-  | Note_on of
-      Audio.Note.t
-      * float (* Note on: note number (A4 = 69), velocity (between 0 and 1). *)
+  | Note_on of Audio.Note.t * float (* Note on: note number (A4 = 69), velocity (between 0 and 1). *)
   | Aftertouch of int * float
-  | Control_change of
-      int * int (* TODO: specific type for common control changes *)
+  | Control_change of int * int (* TODO: specific type for common control changes *)
   | Patch of int
   | Channel_aftertouch of int
   | Pitch of int
@@ -134,8 +131,7 @@ let clear_all buf = buf.data <- []
 
 let clear buf ofs len =
   if ofs = 0 && len = duration buf then clear_all buf
-  else
-    buf.data <- List.filter (fun (t, _) -> t < ofs || t >= ofs + len) buf.data
+  else buf.data <- List.filter (fun (t, _) -> t < ofs || t >= ofs + len) buf.data
 
 (* complement of clear *)
 let extract buf ofs len =
@@ -162,7 +158,8 @@ let blit b1 o1 b2 o2 len =
     clear b2 o2 len;
     extract b1 o1 len;
     translate b1 (o2 - o1);
-    merge b2 b1)
+    merge b2 b1
+  )
 
 let insert b te =
   assert (fst te < duration b);
@@ -266,111 +263,115 @@ module IO = struct
             in
             incr pos;
             let command =
-              if command land 0x80 <> 0 then (
-                status := command;
-                command)
-              else (
-                decr pos;
-                !status)
+              if command land 0x80 <> 0 then
+                (
+                  status := command;
+                  command
+                )
+              else
+                (
+                  decr pos;
+                  !status
+                )
             in
             let cmd = (command lsr 4) land 0xf in
             let chan = command land 0xf in
             match cmd with
-              | 8 ->
-                  let n = get_byte () in
-                  let v = get_byte () in
-                  (Some chan, Note_off (n, float v /. 127.))
-              | 9 ->
-                  let n = get_byte () in
-                  let v = get_byte () in
-                  ( Some chan,
-                    if v = 0 then
-                      (* I have seen notes at 0. used as note off...... *)
-                      Note_off (n, 0.)
-                    else Note_on (n, float v /. 127.) )
-              | 0xa ->
-                  let n = get_byte () in
-                  let v = get_byte () in
-                  (Some chan, Aftertouch (n, float v /. 127.))
-              | 0xb ->
-                  let c = get_byte () in
-                  let v = get_byte () in
-                  (Some chan, Control_change (c, v))
-              | 0xc ->
-                  let p = get_byte () in
-                  (Some chan, Patch p)
-              | 0xd ->
-                  let c = get_byte () in
-                  (Some chan, Channel_aftertouch c)
-              | 0xe ->
-                  let l = get_byte () land 0x7f in
-                  let h = get_byte () land 0x7f in
-                  (Some chan, Pitch ((h lsl 7) + l))
-              | _ -> (
-                  match command with
-                    | 0xf0 | 0xf7 ->
-                        (* SysEx *)
-                        let len = read_delta () in
-                        advance len;
-                        raise Not_found
-                    | 0xff -> (
-                        let cmd = get_byte () in
-                        let len = read_delta () in
-                        match cmd with
-                          | 0 ->
-                              if len <> 2 then raise Invalid_data;
-                              let h = get_byte () in
-                              let l = get_byte () in
-                              (None, Sequence_number ((h lsl 8) + l))
-                          | 1 -> (None, Text (get_text len))
-                          | 2 -> (None, Copyright (get_text len))
-                          | 3 -> (None, Track_name (get_text len))
-                          | 4 -> (None, Instrument_name (get_text len))
-                          | 5 -> (None, Lyric (get_text len))
-                          | 6 -> (None, Marker (get_text len))
-                          | 7 -> (None, Cue (get_text len))
-                          | 0x2f (* End of track *) ->
-                              if len <> 0 then raise Invalid_data;
-                              raise Not_found
-                          | 0x51 (* Tempo in microseconds per quarter note *) ->
-                              if len <> 3 then raise Invalid_data;
-                              let t1 = get_byte () in
-                              let t2 = get_byte () in
-                              let t3 = get_byte () in
-                              let t = (t1 lsl 16) + (t2 lsl 8) + t3 in
-                              (None, Tempo t)
-                          | 0x58 (* Time signature *) ->
-                              if len <> 4 then raise Invalid_data;
-                              (* numerator,
-                               * denominator,
-                               * ticks in a metronome click,
-                               * 32nd notes to the quarter note *)
-                              let n = get_byte () in
-                              let d = get_byte () in
-                              let c = get_byte () in
-                              let b = get_byte () in
-                              (None, Time_signature (n, d, c, b))
-                          | 0x59 (* Key signature *) ->
-                              if len <> 2 then raise Invalid_data;
-                              let sf = get_byte () in
-                              (* sharps / flats *)
-                              let m = get_byte () in
-                              (* minor? *)
-                              (None, Key_signature (sf, m <> 0))
-                          | 0x54 (* SMPTE Offset *)
-                          | 0x7f (* Sequencer-specific data *) ->
-                              advance len;
-                              raise Not_found
-                          | _ ->
-                              advance len;
-                              Printf.printf "MIDI: unknown meta-event %x.\n%!"
-                                cmd;
-                              raise Not_found)
+            | 8 ->
+              let n = get_byte () in
+              let v = get_byte () in
+              (Some chan, Note_off (n, float v /. 127.))
+            | 9 ->
+              let n = get_byte () in
+              let v = get_byte () in
+              ( Some chan,
+                if v = 0 then
+                  (* I have seen notes at 0. used as note off...... *)
+                  Note_off (n, 0.)
+                else Note_on (n, float v /. 127.) )
+            | 0xa ->
+              let n = get_byte () in
+              let v = get_byte () in
+              (Some chan, Aftertouch (n, float v /. 127.))
+            | 0xb ->
+              let c = get_byte () in
+              let v = get_byte () in
+              (Some chan, Control_change (c, v))
+            | 0xc ->
+              let p = get_byte () in
+              (Some chan, Patch p)
+            | 0xd ->
+              let c = get_byte () in
+              (Some chan, Channel_aftertouch c)
+            | 0xe ->
+              let l = get_byte () land 0x7f in
+              let h = get_byte () land 0x7f in
+              (Some chan, Pitch ((h lsl 7) + l))
+            | _ -> (
+                match command with
+                | 0xf0 | 0xf7 ->
+                  (* SysEx *)
+                  let len = read_delta () in
+                  advance len;
+                  raise Not_found
+                | 0xff -> (
+                    let cmd = get_byte () in
+                    let len = read_delta () in
+                    match cmd with
+                    | 0 ->
+                      if len <> 2 then raise Invalid_data;
+                      let h = get_byte () in
+                      let l = get_byte () in
+                      (None, Sequence_number ((h lsl 8) + l))
+                    | 1 -> (None, Text (get_text len))
+                    | 2 -> (None, Copyright (get_text len))
+                    | 3 -> (None, Track_name (get_text len))
+                    | 4 -> (None, Instrument_name (get_text len))
+                    | 5 -> (None, Lyric (get_text len))
+                    | 6 -> (None, Marker (get_text len))
+                    | 7 -> (None, Cue (get_text len))
+                    | 0x2f (* End of track *) ->
+                      if len <> 0 then raise Invalid_data;
+                      raise Not_found
+                    | 0x51 (* Tempo in microseconds per quarter note *) ->
+                      if len <> 3 then raise Invalid_data;
+                      let t1 = get_byte () in
+                      let t2 = get_byte () in
+                      let t3 = get_byte () in
+                      let t = (t1 lsl 16) + (t2 lsl 8) + t3 in
+                      (None, Tempo t)
+                    | 0x58 (* Time signature *) ->
+                      if len <> 4 then raise Invalid_data;
+                      (* numerator,
+                       * denominator,
+                       * ticks in a metronome click,
+                       * 32nd notes to the quarter note *)
+                      let n = get_byte () in
+                      let d = get_byte () in
+                      let c = get_byte () in
+                      let b = get_byte () in
+                      (None, Time_signature (n, d, c, b))
+                    | 0x59 (* Key signature *) ->
+                      if len <> 2 then raise Invalid_data;
+                      let sf = get_byte () in
+                      (* sharps / flats *)
+                      let m = get_byte () in
+                      (* minor? *)
+                      (None, Key_signature (sf, m <> 0))
+                    | 0x54 (* SMPTE Offset *)
+                    | 0x7f (* Sequencer-specific data *) ->
+                      advance len;
+                      raise Not_found
                     | _ ->
-                        advance 1;
-                        Printf.printf "MIDI: unknown command %x (pos: %d)\n%!"
-                          command !pos;
-                        raise Not_found)
+                      advance len;
+                      Printf.printf "MIDI: unknown meta-event %x.\n%!"
+                        cmd;
+                      raise Not_found)
+                | _ ->
+                  advance 1;
+                  Printf.printf "MIDI: unknown command %x (pos: %d)\n%!"
+                    command !pos;
+                  raise Not_found)
           in
           let ans = ref [] in
           while !pos < len do
